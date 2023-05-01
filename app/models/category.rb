f221a4cff920872faf639bbac7e2b595213e6d6a
tobuya <thomasobuya95@gmail.com>
2023-05-01 23:12:46 +0300

    Add associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_many :category_transactions, dependent: :destroy
+  has_many :transactions, through: :category_transactions, source :category
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
