3988e7cb4884d1ff49946b05eeed429b6d30a985
tobuya <thomasobuya95@gmail.com>
2023-05-02 22:29:45 +0300

    Add migrations files and run db migration

---
+++ db/migrate/20230502130543_add_user_to_transactions.rb
@@ -0,0 +1,5 @@
+class AddUserToTransactions < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :transactions, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_181027) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_142514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_181027) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "category_transactions", force: :cascade do |t|
@@ -35,6 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_181027) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_transactions_on_category_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,6 +51,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_181027) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "category_transactions", "categories"
   add_foreign_key "category_transactions", "transactions"
+  add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "users"
 end
