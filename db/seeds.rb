05945cb0a881c0e54e061dd32fdf87a02593551a
tobuya <thomasobuya95@gmail.com>
2023-05-02 12:34:43 +0300

    Add sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Create some sample users
+User.create(name: "John Doe", email: "john@example.com", password_digest: "password")
+User.create(name: "Jane Smith", email: "jane@example.com", password_digest: "password")
+
+# Create some sample categories
+Category.create(name: "Food", icon: "🍔")
+Category.create(name: "Shopping", icon: "🛍️")
+Category.create(name: "Entertainment", icon: "🎬")
+
+# Create some sample transactions
+Transaction.create(name: "Lunch at McDonald's", amount: 7.99)
+Transaction.create(name: "Groceries at Walmart", amount: 35.78)
+Transaction.create(name: "Movie tickets", amount: 12.50)
+
+# Associate transactions with categories
+CategoryTransaction.create(category_id: 1, transaction_id: 1)
+CategoryTransaction.create(category_id: 2, transaction_id: 2)
+CategoryTransaction.create(category_id: 3, transaction_id: 3)
